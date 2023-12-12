e5230b90b72d98cb89ec798c5d5852b5ab25827c
Abdullah Shahed <142104079+shahed32156@users.noreply.github.com>
2023-12-13 00:07:50 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+function toggleContent() {
+    var moreContent = document.getElementById("more-content");
+    var buttonText = document.querySelector("button");
+
+    if (moreContent.style.display === "none") {
+        moreContent.style.display = "block";
+        buttonText.innerHTML = "See less";
+    } else {
+        moreContent.style.display = "none";
+        buttonText.innerHTML = "See more...";
+    }
+}
+
+let btnClear = document.querySelector('.reset');
+let inputs = document.querySelectorAll('input');
+let msg = document.querySelectorAll('textarea');
+
+btnClear.addEventListener('click', () => {
+    inputs.forEach(input =>  input.value = '');
+    msg.forEach(textarea =>  textarea.value = '');
+});
+
+function white() {
+
+    document.getElementsByTagName('button').style.color = "red";
+}
+
+
+var content = document.body;
+var darkMode = document.getElementById('dark-change');
+darkMode.addEventListener('click', function(){
+    darkMode.classList.toggle('active');
+    content.classList.toggle('night');
+    if(content.classList.contains("night")){
+        localStorage.setItem("theme","night");
+    }else{
+        localStorage.setItem("theme","");
+    }
+})
+function themeUpdate(){
+    if(localStorage.getItem("theme")!==null){
+        if(localStorage.getItem("theme") == "night"){
+            content.classList.add("night");
+        darkMode.classList.add('active');
+        }else{
+            content.classList.remove("night");
+        darkMode.classList.remove('active');
+        }
+    }
+}
+
+themeUpdate();
+
